86e767b5ecb1cf4962c6ebde2ddbf5909b0f8c2b
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-08-06 22:35:13 +0300

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,162 +1,46 @@
-import gallery from './gallery-items.js'
 
-const openModal = document.querySelector('.js-lightbox');
-const closeBtn = document.querySelector('.lightbox__button');
-const overlay = document.querySelector('.lightbox__content');
-
-
-overlay.addEventListener('click', onOverlayClose)
-closeBtn.addEventListener('click', onModalClose)
+const daysRef = document.querySelector('.value[data-value="days"]');
+const hoursRef = document.querySelector('.value[data-value="hours"]');
+const minsRef = document.querySelector('.value[data-value="mins"]');
+const secsdRef = document.querySelector('.value[data-value="secs"]');
 
 
-const createSetOfPictures = (data) => {
-    const itemRef = document.createElement('li')
-    itemRef.classList.add('gallery__item')
-    const linkRef = document.createElement('a')
-    linkRef.classList.add('gallery__link')
-    linkRef.setAttribute('href', data.preview)
-    const imgRef = document.createElement('img')
-    imgRef.classList.add('gallery__image')
-    imgRef.setAttribute('src', data.original)
-    imgRef.setAttribute('data-source', data.original)
-    imgRef.setAttribute('alt', data.description)
-    linkRef.appendChild(imgRef)
-    itemRef.appendChild(linkRef)
-    return itemRef;
-}
-const data = gallery.map(item => createSetOfPictures(item))
 
-const ulRef = document.querySelector('.js-gallery');
-ulRef.append(... data)
+const timer = {
+    start(){
+        const startTime = new Date(2020, 7, 22, 9, 0, 0, 0);;
+        setInterval(()=> {
+            const currentTime = Date.now();
+            const deltaTime = startTime - currentTime ;
+        
+            updateClockFace(deltaTime)
 
-ulRef.addEventListener('click', openPicture)
-
-
-function openPicture(event) {
-    event.preventDefault()
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    onModalOpen(event.target)
-}
-
-
-function onModalOpen(clickedTarget) {
-    if (clickedTarget) {
-        openModal.classList.add('is-open')
-    }
-    modalSetup (clickedTarget)
-}
-
-function modalSetup (clickedTarget){
-    const currentPicture = openModal.querySelector('.lightbox__image');
-    const imgAlt = clickedTarget.getAttribute('alt')
-    currentPicture.setAttribute('src', clickedTarget.dataset.source)
-    currentPicture.setAttribute('alt', imgAlt)
-}
-
-function onModalClose(clickedTarget) {
-    if (clickedTarget) {
-        openModal.classList.remove('is-open')
-    }
-    modalClear()
-}
-function modalClear(){
-    const clearData = openModal.querySelector('.lightbox__image');
-    clearData.setAttribute('src', '')
-    clearData.setAttribute('alt', '')
-}
-
-function onOverlayClose(event) {
-
-    if (event.target.nodeName !== 'IMG') {
-        onModalClose(event)
+        }, 1000)
     }
 }
 
+timer.start()
 
 
 
 
-// const clientW = event.target.clientWidth*0.5
-// if (event.offsetX > clientW){
-//     console.log('nextpage');
-// }else{
-//     console.log('prevpage');
-// }
-// }
+function updateClockFace(time){
 
+const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
-// const openModal = document.querySelector('.js-lightbox');
-// const closeBtn = document.querySelector('.lightbox__button');
-// closeBtn.addEventListener('click', onModalClose)
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
 
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
 
-// const createSetOfPictures =  (data) => {
-//     const itemRef = document.createElement('li')
-//     itemRef.classList.add('gallery__item')
-//     const linkRef = document.createElement('a')
-//     linkRef.classList.add('gallery__link')
-//     linkRef.setAttribute('href', data.preview)
-//     const imgRef = document.createElement('img')
-//     imgRef.classList.add('gallery__image')
-//     imgRef.setAttribute('src', data.original)
-//     imgRef.setAttribute('data-source', data.original)
-//     imgRef.setAttribute('alt', data.description)
-//     linkRef.appendChild(imgRef)
-//     itemRef.appendChild(linkRef)
-//     return itemRef;
-// }
-// const data = gallery.map(item => createSetOfPictures(item))
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-// const ulRef = document.querySelector('.js-gallery');
-// ulRef.append(...data)
+daysRef.textContent = `${days}`
+hoursRef.textContent = `${hours}`
+minsRef.textContent = `${mins}`
+secsdRef.textContent = `${secs}`
 
-// ulRef.addEventListener('click', openPicture)
+}
 
-
-// function openPicture(event){
-//     event.preventDefault()
-//     const clickedTarget = event.target;
-//     if (clickedTarget.nodeName !== 'IMG'){
-//         return;
-//     }
-
-//     onModalOpen(clickedTarget)
-
-//     console.log(clickedTarget);
-//     console.log(clickedTarget.nodeName);
-//     console.log(event.currentTarget);
-// }
-
-
-// function onModalOpen (clickedTarget){
-//     if(clickedTarget){
-//         openModal.classList.add('is-open')
-//         const currentPicture = openModal.querySelector('.lightbox__image');
-//         const imgAlt = clickedTarget.getAttribute('alt')
-//         currentPicture.setAttribute('src', clickedTarget.dataset.source)
-//         currentPicture.setAttribute('alt', imgAlt)
-//     }
-// }
-
-// function onModalClose (clickedTarget){
-//     if(clickedTarget){
-//         openModal.classList.remove('is-open')
-//         const clearUrl = openModal.querySelector('.lightbox__image');
-//         clearUrl.setAttribute('src', '')
-//         clearUrl.setAttribute('alt', '')
-//     }
-// }
-
-
-// =======================================================================================
-
-// const galleryListRef = document.querySelector('.js-gallery');
-
-// const pictures = gallery.reduce((acc, el) => {
-// acc+= `<li class="gallery__link"><a class="gallery__link" href="${el.preview}"><img class="gallery__image" src="${el.preview}" data-sourec ="${el.original}" alt="${el.description}"></a></li>  `
-//     return acc
-// }, '')
-
-// galleryListRef.insertAdjacentHTML('afterbegin', pictures)
+function pad (value){
+    return String(value).padStart(2, '0')
+}
